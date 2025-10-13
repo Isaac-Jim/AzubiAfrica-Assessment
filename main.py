203090bb370e2dd90d3cf4e68686adc3d19ade7a
Isaac-Jim <jimawuahisaac@gmail.com>
2025-10-13 15:40:01 +0000

    new: built end point for api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import  BaseModel
+import joblib
 
 app =  FastAPI()
 
@@ -32,3 +33,48 @@ class   subscription_features(BaseModel):
     day_of_week_tue         :bool
     day_of_week_wed         :bool
     contact_telephone       :bool
+
+# creating an end point for prediction
+
+@app.post("predict_subscription")
+def  predict (data :subscription_features):
+    features =[[
+
+    data.age,
+    data.job , 
+    data.education ,  
+    data.duration , 
+    data.campaign ,     
+    data.pdays ,         
+    data.previous ,   
+    data.emp_var_rate,        
+    data.cons_price_idx,     
+    data.cons_conf_idx,      
+    data.euribor3m   ,       
+    data.nr_employed  ,       
+    data.marital_married,      
+    data.marital_single ,        
+    data.marital_unknown ,       
+    data.default_unknown  ,       
+    data.default_yes       ,      
+    data.housing_unknown    ,     
+    data.housing_yes         ,    
+    data.loan_unknown         ,   
+    data.loan_yes              , 
+    data.poutcome_nonexistent   , 
+    data.poutcome_success        ,
+    data.day_of_week_mon         ,
+    data.day_of_week_thu         ,
+    data.day_of_week_tue         ,
+    data.day_of_week_wed        ,
+    data.contact_telephone 
+
+    ]]
+
+    #load model
+
+    model = joblib.load("Random_Forest_1.0")
+
+    results = model.predict(features)[0]
+
+    return {f"subscription package indicates ":{results}}
